48ad2cd975447991e75c3f5950124dc280f65ba8
Empty Arsenal <matt.merkes@gmail.com>
2013-03-31 18:38:25 -0700

    program now writes all of the attendees to log.txt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app = require('express')()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , fs = require('fs');
 
 server.listen(process.env.PORT || 8008);
 
@@ -29,6 +30,11 @@ io.sockets.on('connection', function (socket) {
         }   
       } 
       if(unique) {
+        var user = data.firstName + ' ' + data.lastName + ' ' + data.email + '\n';
+        fs.appendFile('log.txt', user, function (err) {
+          if (err) throw err;
+          console.log('A new attendee was added to the log');
+        });
         attendees.push(data); 
         console.log(attendees); // write to a file?
         // we tell the client to execute 'updateAttendees' with 3 parameters
